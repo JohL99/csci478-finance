b39960e434daeee108a3bbf254ece9205ba180dc
johl99 <jlamprec16@gmail.com>
2024-10-03 19:30:18 -0700

    added detailed print statements

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def fetchAndSaveData():
 
 
 if __name__ == "__main__":
-    print("starting...\n\n")
+    print("starting...\n")
     
     # get data from yfinance and save it to a JSON file
     fetchAndSaveData()
@@ -41,10 +41,22 @@ if __name__ == "__main__":
     
     # Evaluate the model
     mse = reg_model.evaluate()
-    print(f"Mean Squared Error: {mse}")
+    print(f"Mean Squared Error: {mse}\n")
     
     # Example of predicting the closing price using standardized features
     example_features = pd.DataFrame([[430, 435, 420, 1000000]], columns=['Open', 'High', 'Low', 'Volume'])  # Example features
     predicted_closing_price = reg_model.predict(example_features)
-    print(f"Predicted Closing Price: {predicted_closing_price[0]}")
+    print(f"Predicted Closing Price: {predicted_closing_price[0]}\n")
     
+    opening_price = example_features['Open'][0]
+    predicted_closing_price_value = predicted_closing_price.item()
+    
+    price_change = abs(predicted_closing_price_value - opening_price)
+    
+    if predicted_closing_price < opening_price:
+        print(f"Price went down: Opening Price = {opening_price}, Predicted Closing Price = {predicted_closing_price}, Change = {price_change:.2f}")
+    elif predicted_closing_price > opening_price:
+        print(f"Price went up: Opening Price = {opening_price}, Predicted Closing Price = {predicted_closing_price}, Change = {price_change:.2f}")
+    else:
+        print(f"No price change: Opening Price = {opening_price}, Predicted Closing Price = {predicted_closing_price}")
+        
